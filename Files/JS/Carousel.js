69c874c3d3bfd9eb56705d6d83cc3f8eac248756
GauravDhawale <67972055+GauravDhawale@users.noreply.github.com>
2021-12-28 13:08:04 +0530

    Version 5

--- Files/JS/Carousel.js
+++
@@ -1,9 +0,0 @@
-var left=document.getElementById("leftslide");
-var right=document.getElementById("rightslide");
-var Scrolldiv=document.getElementById("ScrollC");
-left.addEventListener("click",function(){
-    Scrolldiv.scrollLeft-=document.getElementsByClassName("RT")[0].offsetWidth;
-});
-right.addEventListener("click",function(){
-    Scrolldiv.scrollLeft+=document.getElementsByClassName("RT")[0].offsetWidth;
-});
